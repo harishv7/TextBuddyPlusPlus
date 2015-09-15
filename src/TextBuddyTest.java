5803eb2b575926b561e6d165d3defc07258d278f
Harish V <harishv207@gmail.com>
2015-09-16 00:03:51 +0800

    added second test case for sort functionality: test sort after adding some lines/tasks

--- src/TextBuddyTest.java
+++ src/TextBuddyTest.java
@@ -15,12 +15,37 @@ public class TextBuddyTest {
 		TextBuddy.getFileReady();
 	}
 	
-	// Test Sort Case 1: Sort Alphabetical Order
+	// Test Sort Method 1: Sort an empty file
 	@Test
-	public void testSortFile() throws IOException {
+	public void testSortEmptyFile() throws IOException {
 		// test sorting an empty file
 		TextBuddy.clearFile();
 		assertEquals(fileName + " has nothing to sort", TextBuddy.sortFileContents());
 	}
+	
+	// Test Sort Method 2: Sort after adding statements/tasks
+	@Test
+	public void testSortAfterAdd() throws IOException {
+		TextBuddy.clearFile();
+		
+		// add sample lines
+		TextBuddy.addToFile("Camel");
+		TextBuddy.addToFile("Penguin");
+		TextBuddy.addToFile("Buffalo");
+		TextBuddy.addToFile("Hippopotamus");
+		TextBuddy.addToFile("Eagle");
+		TextBuddy.addToFile("Meerkat");
+		TextBuddy.addToFile("Reindeer");
+		TextBuddy.addToFile("Zebra");
+		TextBuddy.addToFile("Shark");
+		TextBuddy.addToFile("Gorilla");
+		TextBuddy.addToFile("Rabbit");
+		TextBuddy.addToFile("Koala");
+		TextBuddy.addToFile("Flamingo");
+		TextBuddy.addToFile("Elephant");
+		
+		assertEquals(fileName + " has been sorted alphabetically", TextBuddy.sortFileContents());
+	}
+	
 	
 }
