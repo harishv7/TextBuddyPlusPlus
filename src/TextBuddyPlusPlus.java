8a9a34619427f1cec24f84c1bb0aa48b44e47a53
Harish V <harishv207@gmail.com>
2015-09-15 22:00:45 +0800

    Main method

---
+++ src/TextBuddyPlusPlus.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class TextBuddyPlusPlus {
+	
+	public static TextBuddy textBuddy = new TextBuddy();
+	
+	public static void main(String[] args) throws IOException {
+		textBuddy.startProgram(args);
+
+	}
+}
